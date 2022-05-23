a422507960500d773ba39cc5de15c53e09427439
Akash Ramesh <akash.briselle@gmail.com>
2022-05-24 00:40:30 +0530

    Products Page

--- buyzack/src/App.jsx
+++ buyzack/src/App.jsx
@@ -1,7 +1,9 @@
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import SingleProduct from "./pages/SingleProduct";
+
 const App = () => {
-  return <ProductList />;
+  return <SingleProduct />;
 };
 
 export default App;
