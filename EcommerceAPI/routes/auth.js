debd08104382ea042c2775c58a427190d54f6916
Akash Ramesh <akash.briselle@gmail.com>
2022-06-01 21:56:18 +0530

    Update,delete,function is implemented

--- EcommerceAPI/routes/auth.js
+++ EcommerceAPI/routes/auth.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJs = require("crypto-js");
+const jwt = require("jsonwebtoken");
+
 //Register
 
 router.post("/register", async (req, res) => {
@@ -21,6 +23,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
+//Login
 router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ username: req.body.username });
@@ -35,8 +38,18 @@ router.post("/login", async (req, res) => {
     OriginalPassword !== req.body.password &&
       res.status(401).json("Wrong credentials");
 
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: "3d" }
+    );
+
     const { password, ...others } = user._doc;
-    res.status(500).json(user);
+
+    res.status(200).json({ ...others, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
--- EcommerceAPI/routes/user.js
+++ EcommerceAPI/routes/user.js
@@ -1,3 +1,43 @@
 const router = require("express").Router();
+const User = require("../models/User");
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+//UpdateUser
+
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  if (req.body.password) {
+    req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString();
+  }
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//DeleteUser
+
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User Has been deleted");
+  } catch (er) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
